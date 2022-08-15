d8550626140ad64f2585165d162da8a4cd7955f8
Ernani Soares Jr <108825177+ErnaniSoaresJr@users.noreply.github.com>
2022-08-15 14:37:58 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const resultado = document.querySelector(' .result ');
+const confirmar = document.querySelector(' .igual ');
+
+function inserir(valor) {
+    resultado.innerHTML += valor;
+}
+
+
+function limpar() {
+    resultado.innerHTML = ' ';
+}
+
+
+function voltar() {
+    if (resultado.textContent) {
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length - 1);
+    }
+}
+
+
+function confirma() {
+    if (resultado.textContent != 'Erro') {
+        document.getElementById('resultado').innerHTML = eval(resultado.innerHTML)
+    }
+}
